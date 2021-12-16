23f517a43e53f948556299664943a7a1e05bb2be
Ragnarok <87938973+Ragnarok360@users.noreply.github.com>
2021-12-16 19:02:18 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -110,7 +110,7 @@ async def start(client, message):
             f_caption=f_caption
     if f_caption is None:
         f_caption = f"{files.file_name}"
-buttons = [
+    buttons = [
         [
              InlineKeyboardButton('♻ Join Group ♻', url='t.me/SS07MOVIEHUT') 
         ]
@@ -119,6 +119,7 @@ buttons = [
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
+        reply_markup=InlineKeyboardMarkup(buttons)
         )
                     
 
