91deecb43f739b41435a604c6cc9460cc837149a
Smptech3938 <93422140+Smptech3938@users.noreply.github.com>
2022-01-09 05:21:33 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -38,7 +38,7 @@ async def save_group(bot, message):
             return
         buttons = [[
             InlineKeyboardButton('ℹ️ Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            InlineKeyboardButton('♻ Group ♻', url='https://t.me/SS07MOVIEHUT')
+            InlineKeyboardButton('♻ Group ♻', url='https://t.me/MOVIESTALKIES23')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
