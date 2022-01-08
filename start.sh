589c395d3dbf4bc29bd9c1a4dbdc2d9a573e0ded
Smptech3938 <93422140+Smptech3938@users.noreply.github.com>
2022-01-08 19:33:55 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Ragnarok360/Lucifer /EvaMaria
+  git clone https://github.com/Smptech3938/AUTO-EVA-V5 /EvaMaria
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /EvaMaria
