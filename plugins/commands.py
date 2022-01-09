3d8efde6631984321830ed8df4e046bbcdade863
Smptech3938 <93422140+Smptech3938@users.noreply.github.com>
2022-01-09 06:01:22 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -85,7 +85,7 @@ async def start(client, message):
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
             InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('♻ Group ♻', url='https://t.me/MOVIESTALKIES23)
+            InlineKeyboardButton('♻ Group ♻', url='https://t.me/MOVIESTALKIES23')
             ],[
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
             InlineKeyboardButton('😊 About', callback_data='about')
